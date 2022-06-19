03e7557027d37ab21698d04b4be31b5b386b701b
shukeel <50740328+shukeel@users.noreply.github.com>
2022-06-19 23:56:00 +0300

    modified

--- app/Http/Livewire/ContactFormCode.php
+++ app/Http/Livewire/ContactFormCode.php
@@ -9,6 +9,22 @@ class ContactFormCode extends Component
     public $name;
     public $email;
     public $message;
+    protected $rules = [
+
+        'name' => 'required|min:6',
+
+        'email' => 'required|email',
+
+        'message' => 'required|min:10',
+
+    ];
+
+    public function formSubmit() 
+    {
+        $this->validate();
+
+        // dd('form submited');
+    }
     public function render()
     {
         return view('livewire.contact-form-code');
--- resources/views/livewire/contact-form-code.blade.php
+++ resources/views/livewire/contact-form-code.blade.php
@@ -1,18 +1,19 @@
-<form class="space-y-4">
+<form wire:submit.prevent='formSubmit' class="space-y-4">
+    {{ $name }}
     <div>
         <x-jet-label for="name" value="{{ __('Name') }}" />
         <x-jet-input wire:model='name' name="name" id="name" type="text" /> 
-        @error('name')<span>{{ $message}}</span>@enderror      
+        @error('name')<span class="text-xs text-red-500" >{{ $message}}</span>@enderror      
     </div>
     <div>
         <x-jet-label for="email" value="{{ __('Email') }}" />
         <x-jet-input wire:model='email' name="email" id="email" type="email" />  
-        @error('email')<span>{{ $message}}</span>@enderror      
+        @error('email')<span class="text-xs text-red-500" >{{ $message}}</span>@enderror      
     </div>
     <div>
         <x-jet-label for="message" value="{{ __('Message') }}" />
-        <textarea wire:model='message' class="w-full" name="message" id="message" cols="30" rows="10"></textarea>
-        @error('message')<span>{{ $message}}</span>@enderror 
+        <textarea wire:model='message' class="w-full" name="message" id="message" cols="20" rows="5"></textarea>
+        @error('message')<span class="text-xs text-red-500" >{{ $message}}</span>@enderror 
     </div>
 
     <x-jet-button type="submit" class="mt-5">
